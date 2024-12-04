4bb7c40d13c24decc4bc0a2553c79df3d95d6942
EstebanPastrana <esteban.pastrana@ulatina.net>
2024-12-04 11:37:53 -0600

    Epastrana,  se termino el pie de pagina dinamico

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".buyButton");
 const cart = document.getElementById('cart');
+var isNewFooterCreated = false;
 
     buttons.forEach(button => {
         button.addEventListener('click', (event) => {
@@ -22,6 +23,9 @@ function addToCart(id, name, price){
     };
 
     cart.appendChild(CreateNewRow(id, name, price));
+
+    checkIsCartIsEmpty();
+    CheckTotalAmountAndPrice();
 }
 
 function CreateNewRow(id, name, price ){
@@ -83,14 +87,103 @@ function IncreaseAmount(row, price){
     const PriceCelda = row.cells[4]; 
     let newTotal = parseInt(PriceCelda.textContent) + parseInt(price);
     PriceCelda.textContent = String(newTotal);
+    CheckTotalAmountAndPrice();
 }
 
 function DecreaseAmount(row, price){
-    const PriceCelda = row.cells[4]; 
-    let newTotal = parseInt(PriceCelda.textContent) - parseInt(price);
-    PriceCelda.textContent = String(newTotal);
+    const PriceCell = row.cells[4]; 
+    let newTotal = parseInt(PriceCell.textContent) - parseInt(price);
+    PriceCell.textContent = String(newTotal);
 
-    const cantidadCelda = row.cells[2]; 
-    cantidadCelda.textContent = cantidadCelda.textContent === '1' ? row.remove() : parseInt(cantidadCelda.textContent) - 1; 
-   
+    const amountCell = row.cells[2]; 
+    amountCell.textContent = amountCell.textContent === '1' ? row.remove() : parseInt(amountCell.textContent) - 1;
+    CheckTotalAmountAndPrice(); 
+    checkIsCartIsEmpty();
+}
+
+function checkIsCartIsEmpty(){
+    
+
+    let footerMessage = document.getElementById('emptyPurchases');
+    const tablefoot = document.getElementById('purchasesFoot');
+
+    let cartLength = cart.rows.length;
+
+    if(cartLength > 0 && !isNewFooterCreated){
+        footerMessage.hidden = true;
+
+        tablefoot.appendChild(GetNewFooter());
+        isNewFooterCreated = true;
+        return;
+    }
+    else if (cartLength === 0 ) {
+
+        footerMessage.hidden = false;
+
+        if (isNewFooterCreated) {
+            const newFooter = document.getElementById('newFooter'); 
+            if (newFooter) {
+                tablefoot.removeChild(newFooter);
+            }
+            isNewFooterCreated = false;
+        }
+    }    
+}
+
+
+function GetNewFooter(){
+
+    let tablefootrow = document.createElement('tr');
+    
+    let tdNewMessage = document.createElement('td');
+    tdNewMessage.textContent = 'Total Producto:'
+    tdNewMessage.colSpan = 2
+
+    let = tdTotalAmount = document.createElement('td');
+    tdTotalAmount.textContent = GetTotal(2);
+    tdTotalAmount.setAttribute('id', 'totalAmount')
+
+    let deleteAllCart = document.createElement('button');
+    deleteAllCart.addEventListener('click', DeleteCart);
+    deleteAllCart.textContent = 'Vaciar Todo'
+    deleteAllCart.classList.add('deleteCartButton')
+
+    let tdTotalPrice = document.createElement('td');
+    tdTotalPrice.textContent = GetTotal(4);
+    tdTotalPrice.setAttribute('id', 'totalPrice')
+
+    tablefootrow.appendChild(tdNewMessage);
+    tablefootrow.appendChild(tdTotalAmount);
+    tablefootrow.appendChild(deleteAllCart);
+    tablefootrow.appendChild(tdTotalPrice);
+
+    tablefootrow.setAttribute('id', 'newFooter')
+
+    return tablefootrow;
+}
+
+function GetTotal(columnIndex){
+
+    let sum = 0;
+    for( const row of cart.rows){
+        let cellValue = parseInt(row.cells[columnIndex].textContent);
+        sum += cellValue;
+    }
+
+    return String(sum);
+}
+
+function DeleteCart(){
+    cart.innerHTML = '';
+    checkIsCartIsEmpty();
+}
+
+function CheckTotalAmountAndPrice(){
+    const totalAmount = document.getElementById('totalAmount');
+    const totalPirce = document.getElementById('totalPrice')
+
+    if(isNewFooterCreated){
+        totalAmount.textContent = GetTotal(2)
+        totalPirce.textContent = GetTotal(4)
+    }
 }
